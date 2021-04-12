0e76994f23a7b0e2ad489520a592ab68167419f4
kdumagalhaes <ce.magalhaespereira@gmail.com>
2021-04-12 15:50:48 -0300

    finished Advantages list component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-    <Price value="$16.00" />
+    <AdvantagesList
+        item1="Unlimited websites"
+        item2="100% data ownership"
+        item3="Email reports"
+    />
 </template>
 
 <script>
 // atoms
-import { Price } from '@/components/atoms'
+import { AdvantagesList } from '@/components/atoms'
 
 export default {
     components: {
-        Price,
+        AdvantagesList,
     },
 }
 </script>
---
+++ src/components/atoms/AdvantagesList.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul class="advantages-list">
+        <li class="advantages-list__item">{{ item1 }}</li>
+        <li class="advantages-list__item">{{ item2 }}</li>
+        <li class="advantages-list__item">{{ item3 }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        item1: String,
+        item2: String,
+        item3: String,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.advantages-list {
+    .advantages-list__item {
+        line-height: 1.8rem;
+        color: $neutral-grayish-blue;
+
+        &::before {
+            content: url('~@/assets/img/icon-check.svg');
+            margin-right: 10px;
+        }
+    }
+}
+</style>

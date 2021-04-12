35c14994310709eff291e6a357bd3b0538346daf
kdumagalhaes <ce.magalhaespereira@gmail.com>
2021-04-12 18:52:15 -0300

    finished Range Slider component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
-    <AdvantagesList
-        item1="Unlimited websites"
-        item2="100% data ownership"
-        item3="Email reports"
-    />
+    <RangeSlider />
 </template>
 
 <script>
 // atoms
-import { AdvantagesList } from '@/components/atoms'
+import { RangeSlider } from '@/components/atoms'
 
 export default {
     components: {
-        AdvantagesList,
+        RangeSlider,
     },
 }
 </script>

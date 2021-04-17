134bbf4fc4af6d15a5d83d2378b01b8787b7a1f1
kdumagalhaes <ce.magalhaespereira@gmail.com>
2021-04-17 17:54:06 -0300

    insert label in the range slider

--- src/components/atoms/RangeSlider.vue
+++ src/components/atoms/RangeSlider.vue
@@ -1,13 +1,17 @@
 <template>
-    <input
-        type="range"
-        name="range-slider"
-        class="range-slider"
-        v-model="rangeSlider"
-        min="0"
-        max="4"
-        :style="{ backgroundImage: createBackgroundString }"
-    />
+    <div class="range">
+        <label for="range">Choose the range views</label>
+        <input
+            id="range"
+            type="range"
+            name="range-slider"
+            class="range-slider"
+            v-model="rangeSlider"
+            min="0"
+            max="4"
+            :style="{ backgroundImage: createBackgroundString }"
+        />
+    </div>
 </template>
 
 <script>
@@ -44,33 +48,43 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.range-slider {
-    -webkit-appearance: none;
+.range {
     width: 100%;
-    max-width: 500px;
-    height: 10px;
-    border-radius: 50px;
-    background: linear-gradient(
-        90deg,
-        $primary-soft-cyan 50%,
-        $neutral-light-grayish-blue 50%
-    );
 
-    &::-webkit-slider-thumb {
-        -webkit-appearance: none;
-        appearance: none;
-        width: 40px;
-        height: 40px;
-        border-radius: 50px;
-        background-color: $primary-strong-cyan;
-        cursor: pointer;
-        background-image: url('~@/assets/img/icon-slider.svg');
-        background-repeat: no-repeat;
-        background-position: center;
+    label {
+        visibility: hidden;
+        position: absolute;
+        left: -10000px;
     }
 
-    &:active::-webkit-slider-thumb {
-        cursor: grabbing;
+    .range-slider {
+        -webkit-appearance: none;
+        width: 100%;
+        max-width: 500px;
+        height: 10px;
+        border-radius: 50px;
+        background: linear-gradient(
+            90deg,
+            $primary-soft-cyan 50%,
+            $neutral-light-grayish-blue 50%
+        );
+
+        &::-webkit-slider-thumb {
+            -webkit-appearance: none;
+            appearance: none;
+            width: 40px;
+            height: 40px;
+            border-radius: 50px;
+            background-color: $primary-strong-cyan;
+            cursor: pointer;
+            background-image: url('~@/assets/img/icon-slider.svg');
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+
+        &:active::-webkit-slider-thumb {
+            cursor: grabbing;
+        }
     }
 }
 </style>
